6d8a703b178061f7c07a90e95e7c0fe107a55bb4
CORPAA\919842 <yelena.gouralnik@aa.com>
2019-05-15 16:00:44 -0700

    Adding populat subjects

--- PrototypeOnePortal/wwwroot/js/site.js
+++ PrototypeOnePortal/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿    // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your Javascript code.
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
